9726d4482ad78ec66b888feadd14541ad91b1e14
Yirey Suh <yrysuh@gmail.com>
2023-05-17 10:19:58 +0900

    add image modal

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Item.css';
 
 const Item = ({ data }) => {
   const { title, discountPercentage, price, image_url } = data;
+
+  const [show, setShow] = useState(false);
   return (
-    <div className="item-box">
-      <div className="item-box-upper">
-        <img src={image_url} />
-      </div>
-      <div className="item-box-lower">
-        <div className="item-box-title">
-          <div>{title}</div>
-          {discountPercentage ? <div>{`${discountPercentage}%`}</div> : null}
+    <>
+      <div className="item-box">
+        <div
+          className="item-box-upper"
+          onClick={() => {
+            setShow(true);
+          }}
+        >
+          <img src={image_url} />
         </div>
-        <div className="item-box-percent">{price}</div>
+        <div className="item-box-lower">
+          <div className="item-box-title">
+            <div>{title}</div>
+            {discountPercentage ? <div>{`${discountPercentage}%`}</div> : null}
+          </div>
+          <div className="item-box-percent">{price}</div>
+        </div>
       </div>
-    </div>
+      {show ? (
+        <div
+          className="image-modal"
+          onClick={() => {
+            setShow(false);
+          }}
+        >
+          <div className="image-modal-box">
+            <img src={image_url} />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
