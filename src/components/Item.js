58c98237c68fd5bc90242c6cf481c15ace5df4a4
Yirey Suh <yrysuh@gmail.com>
2023-05-17 09:50:00 +0900

    add items to main page

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,23 @@
-function Item () {
-       return (
-        <></>
-       )
-}
+import React from 'react';
+
+import './Item.css';
+
+const Item = ({ data }) => {
+  const { title, discountPercentage, price, image_url } = data;
+  return (
+    <div className="item-box">
+      <div className="item-box-upper">
+        <img src={image_url} />
+      </div>
+      <div className="item-box-lower">
+        <div className="item-box-title">
+          <div>{title}</div>
+          {discountPercentage ? <div>{`${discountPercentage}%`}</div> : null}
+        </div>
+        <div className="item-box-percent">{price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
